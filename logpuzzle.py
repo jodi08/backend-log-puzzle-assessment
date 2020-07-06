46dbd2d0062d184fa96ba548b643de56ad903b09
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-07-06 08:37:41 -0700

    finished project, both pic downloading and can view on browser

--- logpuzzle.py
+++ logpuzzle.py
@@ -21,6 +21,7 @@ import urllib.request
 import argparse
 
 
+
 def read_urls(filename):
     """Returns a list of the puzzle URLs from the given log file,
     extracting the hostname from the filename itself, sorting
@@ -28,21 +29,18 @@ def read_urls(filename):
     """
     #open file, read and save as f:
     puzzle_urls = []
+    server = filename[re.search(r"_(.*?)", filename).span()[1]:]
     with open(filename, 'r') as f:
-        read_puzzle_file = f.read().split('\n') #read file
-        for line in read_puzzle_file:
-            pic_files = re.findall(r'GET \S+ HTTP', read_puzzle_file) #search file with regex
-            for path in paths:
-                print(path)
-
-        
-                
-    return 
-
-    read_urls()
-
+        for line in f:
+            url_result = re.findall(r'GET \S+ HTTP', line) #search file with regex
+            for path in url_result:
+                if path[5:-5] not in puzzle_urls and "puzzle" in path:
+                    puzzle_urls.append(path[5:-5])
+            puzzle_urls.sort(key=lambda x: x[-8:-4])
+    puzzle_urls = list(map(lambda each: "http://" + server + "/" + each, puzzle_urls))
+    return puzzle_urls
+    print(puzzle_urls)
     
-
 
 def download_images(img_urls, dest_dir):
     """Given the URLs already in the correct order, downloads
@@ -52,40 +50,54 @@ def download_images(img_urls, dest_dir):
     to show each local image file.
     Creates the directory if necessary.
     """
-    # request url's from web server
-    # put files back together by alphabetizing
+    image_list = []
+    if not os.path.exists(dest_dir):
     # make an index.html file with an img tag to be able to show image
+        os.makedirs(dest_dir)
+        print("Directory made")
+    for index, url in enumerate(img_urls):
+        image_name = dest_dir + "/img" + str(index) + url[-4:]
+        print("Retrieving files {}".format(url))
+    # request url's from web server
+        urllib.request.urlretrieve(url, image_name)
+        image_list.append("img" + str(index) + url[-4:])
+    with open(dest_dir + "/index.html", "a") as f:
+        f.write("<html>")
+        f.write("<body>")
+        for image in image_list:
+            f.write(f"<img src={image}>")
+        f.write("</body>")
+        f.write("</html>")
     
-    pass
 
 
-# def create_parser():
-#     """Creates an argument parser object."""
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('-d', '--todir',
-#                         help='destination directory for downloaded images')
-#     parser.add_argument('logfile', help='apache logfile to extract urls from')
+def create_parser():
+    """Creates an argument parser object."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--todir',
+                        help='destination directory for downloaded images')
+    parser.add_argument('logfile', help='apache logfile to extract urls from')
 
-#     return parser
-
-
-# def main(args):
-#     """Parses args, scans for URLs, gets images from URLs."""
-#     parser = create_parser()
-
-#     if not args:
-#         parser.print_usage()
-#         sys.exit(1)
-
-#     parsed_args = parser.parse_args(args)
-
-#     img_urls = read_urls(parsed_args.logfile)
-
-#     if parsed_args.todir:
-#         download_images(img_urls, parsed_args.todir)
-#     else:
-#         print('\n'.join(img_urls))
+    return parser
 
 
-# if __name__ == '__main__':
-#     main(sys.argv[1:])
+def main(args):
+    """Parses args, scans for URLs, gets images from URLs."""
+    parser = create_parser()
+
+    if not args:
+        parser.print_usage()
+        sys.exit(1)
+
+    parsed_args = parser.parse_args(args)
+
+    img_urls = read_urls(parsed_args.logfile)
+
+    if parsed_args.todir:
+        download_images(img_urls, parsed_args.todir)
+    else:
+        print('\n'.join(img_urls))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
